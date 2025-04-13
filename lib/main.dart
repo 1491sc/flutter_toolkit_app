5a36b8e1f3150c086527a0a1b1b612f0c455edde
1491sc <stephenc0530@gmail.com>
2025-04-13 13:09:57 +0800

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,45 @@
+
+import 'package:flutter/material.dart';
+import 'package:webview_flutter/webview_flutter.dart';
+
+void main() => runApp(const MyApp());
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: WebApp(),
+    );
+  }
+}
+
+class WebApp extends StatefulWidget {
+  const WebApp({super.key});
+
+  @override
+  State<WebApp> createState() => _WebAppState();
+}
+
+class _WebAppState extends State<WebApp> {
+  late final WebViewController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = WebViewController()
+      ..loadRequest(Uri.parse('https://adia1491.com/yzf168'))
+      ..setJavaScriptMode(JavaScriptMode.unrestricted);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: SafeArea(
+        child: WebViewWidget(controller: _controller),
+      ),
+    );
+  }
+}
